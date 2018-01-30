b29a76c54dcbab203972ec9708085123f311cb66
Anthony Bangrazi <abangrazi@acuantcorp.com>
2018-01-30 11:08:04 -0500

    change UI to include prompt

--- Program.cs
+++ Program.cs
@@ -10,14 +10,16 @@ namespace LicensingPassword
         {
             Console.WriteLine("Licensing Password");
             string input;
+            Console.WriteLine("Enter a password:");
             while(!String.IsNullOrWhiteSpace(input = Console.ReadLine())) {
-                Console.WriteLine();
                 Console.WriteLine($"input={input}");
                 using (SHA256CryptoServiceProvider provider = new SHA256CryptoServiceProvider())
                 {
                     provider.ComputeHash(Encoding.ASCII.GetBytes(input));
                     string password = Convert.ToBase64String(provider.Hash);
                     Console.WriteLine($"password={password}");
+                    Console.WriteLine();
+                    Console.WriteLine("Enter a password:");
                 }
             }
         }

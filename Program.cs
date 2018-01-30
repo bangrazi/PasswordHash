91f460449e6757924b98b89e46f4092749fe3638
Anthony Bangrazi <abangrazi@acuantcorp.com>
2018-01-30 10:31:27 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LicensingPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Licensing Password");
+            string input;
+            while(!String.IsNullOrWhiteSpace(input = Console.ReadLine())) {
+                Console.WriteLine();
+                Console.WriteLine($"input={input}");
+                using (SHA256CryptoServiceProvider provider = new SHA256CryptoServiceProvider())
+                {
+                    provider.ComputeHash(Encoding.ASCII.GetBytes(input));
+                    string password = Convert.ToBase64String(provider.Hash);
+                    Console.WriteLine($"password={password}");
+                }
+            }
+        }
+    }
+}
